e06ba2b357b122f34f211884beff22bcdb05ba2d
Zayd Alameddine <zayd.alameddine@gmail.com>
2020-01-28 15:45:11 -0500

    Uncommenting
    commenting was done for testing undoing those changes

--- User_Churn_Project/User_Churn.sql
+++ User_Churn_Project/User_Churn.sql
@@ -1,18 +1,18 @@
 
 /*Querying for the first 100 rows to understand
 the layout of the table */
-/*
+
 SELECT *
 FROM subscriptionsData
 LIMIT 100;
-*/
+
 
 /*Determining the range of months of data provided
 */
-/*
+
 SELECT MIN(subscription_start), MAX(subscription_start)
 FROM subscriptionsData;
-*/
+
 
 /*There are two segments in this data that will
 be compared based on their churn rates. Below
